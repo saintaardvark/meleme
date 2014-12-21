cbeb04a4342f7dd76ef1d117cff3efa2fc5429ec
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-12-20 17:03:55 -0800

    Stub for analyzing each element.

--- meleme.el
+++ meleme.el
@@ -13,6 +13,7 @@
       (while list
         ;; Hm: Have to use print to make this work with with-output-to-temp-buffer.
         (print (format "%s\n" (car list)))
+        (print (format "%s\n" (meleme-analyze (car list))))
         (setq list (cdr list))))))
 
 (defun meleme-display()
@@ -20,6 +21,11 @@
   (interactive)
   (with-output-to-temp-buffer "*meleme*"
     (meleme)))
+
+(defun meleme-analyze (element)
+  "Display what each ELEMENT of the mode format does."
+  (interactive "S")
+  (quote "FIXME: Made it here"))
 
 (provide 'meleme)
 ;;; meleme ends here
